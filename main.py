c80b96007162c4d90e7e5fb478d9ff91d7dd2be8
vlad <vlad2003voit48gmail.com@MacBook-Air-vlad.local>
2025-06-15 15:17:41 +0700

    Обновил main.py

--- main.py
+++ main.py
@@ -1,32 +1,16 @@
-<<<<<<< HEAD
 from fastapi import FastAPI, Depends, Query
-=======
-from fastapi import FastAPI, Depends, HTTPException, Query
->>>>>>> cf8039a313634c9477b480605ffd3a7376d3ce4d
 from sqlalchemy.ext.asyncio import AsyncSession
 from sqlalchemy.future import select
 from models import Game, Provider
 from database import get_db
-<<<<<<< HEAD
 from typing import Optional, List
-=======
-from typing import Optional
->>>>>>> cf8039a313634c9477b480605ffd3a7376d3ce4d
 from pydantic import BaseModel
 
 app = FastAPI()
 
-<<<<<<< HEAD
 class SearchResult(BaseModel):
     games: List[int] = []
     providers: List[int] = []
-=======
-
-class SearchResult(BaseModel):
-    games: list[int] = []
-    providers: list[int] = []
-
->>>>>>> cf8039a313634c9477b480605ffd3a7376d3ce4d
 
 @app.get("/search/", response_model=SearchResult)
 async def search(
@@ -36,7 +20,6 @@ async def search(
     result = SearchResult()
 
     if query:
-<<<<<<< HEAD
         
         games_stmt = select(Game.id).where(Game.title.ilike(f"%{query}%"))
         games_result = await db.execute(games_stmt)
@@ -46,16 +29,5 @@ async def search(
         providers_stmt = select(Provider.id).where(Provider.name.ilike(f"%{query}%"))
         providers_result = await db.execute(providers_stmt)
         result.providers = [row[0] for row in providers_result.fetchall()]
-=======
-        # Поиск игр
-        games_stmt = select(Game.id).where(Game.title.ilike(f"%{query}%"))
-        games_result = await db.execute(games_stmt)
-        result.games = [row[0] for row in games_result]
-
-        # Поиск провайдеров
-        providers_stmt = select(Provider.id).where(Provider.name.ilike(f"%{query}%"))
-        providers_result = await db.execute(providers_stmt)
-        result.providers = [row[0] for row in providers_result]
->>>>>>> cf8039a313634c9477b480605ffd3a7376d3ce4d
 
     return result
